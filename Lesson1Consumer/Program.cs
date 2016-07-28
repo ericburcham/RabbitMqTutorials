356790bb22c7e5ebdfae5de70375192425517482
Eric Burcham <jameseburcham@gmail.com>
2016-07-28 03:01:04 -0500

    Finished first tutorial.

---
+++ Lesson1Consumer/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Lesson1Consumer
+{
+    public class Program
+    {
+        static void Main()
+        {
+            var connectionFactory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                UserName = "admin",
+                Password = "Isaiah_60"
+            };
+
+            using (var connection = connectionFactory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(
+                        queue: "Lesson1Producer",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null);
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine($" [x] Received {message}");
+                    };
+
+                    channel.BasicConsume(
+                        queue: "Lesson1Producer",
+                        noAck: true,
+                        consumer: consumer);
+
+                    Console.WriteLine(value: " Press [enter] to exit.");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
---
+++ Lesson1Producer/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Lesson1Producer
+{
+    public class Program
+    {
+        static void Main()
+        {
+            var connectionFactory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                UserName = "admin",
+                Password = "Isaiah_60"
+            };
+
+            const string message = "Hello World!";
+            using (var connection = connectionFactory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(
+                        queue: "Lesson1Producer",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null);
+
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(
+                        exchange: "",
+                        routingKey: "Lesson1Producer",
+                        basicProperties: null,
+                        body: body);
+
+                    Console.WriteLine($" [x] Sent {message}");
+                }
+            }
+
+            Console.WriteLine(value: " Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
